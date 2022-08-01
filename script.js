2d8e8f9e973b7369491c5d3c673156b9311e996d
Ramesh Penjerla <rameshpenjerla@gmail.com>
2022-08-01 11:22:06 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
+let data = new Object();
+
 let firstName = document.getElementById('fname').value;
+data.firstName = firstName;
 let lastName = document.getElementById('lname').value;
+data.lastName = lastName;
 let address = document.getElementById('address').value;
+data.address = address;
 let state = document.getElementById('state').value;
+data.state = state;
 let country = document.getElementById('country').value;
+data.country = country;
 let pin = document.getElementById('pin').value;
+data.pin = pin;
 
 let gender = document.querySelector('input[name="Gender"]:checked').value;
+data.gender = gender;
 
 let foods = document.getElementsByName('food[]');
-
 let food = [];
 for(let i=0; i<foods.length; i++) {
     if(foods[i].checked){
         food.push(foods[i]);
     }
 };
+data.food = food;
 
-console.log(firstName,lastName,address,state,country,pin,gender,food);
+
+console.log(data);
 
 
 
