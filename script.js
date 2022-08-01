de7318179ce7f864a27d78b6206ba162876819ca
Ramesh Penjerla <rameshpenjerla@gmail.com>
2022-08-01 23:01:19 +0530

    update changes

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
+
+
 let data = new Object();
 
-let firstName = document.getElementById('fname').value;
-data.firstName = firstName;
-let lastName = document.getElementById('lname').value;
-data.lastName = lastName;
-let address = document.getElementById('address').value;
-data.address = address;
-let state = document.getElementById('state').value;
-data.state = state;
-let country = document.getElementById('country').value;
-data.country = country;
-let pin = document.getElementById('pin').value;
-data.pin = pin;
-
-let gender = document.querySelector('input[name="Gender"]:checked').value;
-data.gender = gender;
-
-let foods = document.getElementsByName('food[]');
-let food = [];
-for(let i=0; i<foods.length; i++) {
-    if(foods[i].checked){
-        food.push(foods[i]);
-    }
-};
-data.food = food;
 
 
 
@@ -34,6 +12,34 @@ let form = document.getElementById('form');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+    let firstName = document.querySelector('#fname').value;
+    data.firstName = firstName;
+    let lastName = document.getElementById('lname').value;
+    data.lastName = lastName;
+    let address = document.getElementById('address').value;
+    data.address = address;
+    let state = document.getElementById('state').value;
+    data.state = state;
+    let country = document.getElementById('country').value;
+    data.country = country;
+    let pin = document.getElementById('pin').value;
+    data.pin = pin;
+    
+    let gender = document.querySelector('input[name="Gender"]:checked').value;
+    data.gender = gender;
+    
+    let foods = document.getElementsByName('food[]');
+    let food = [];
+    for(let i=0; i<foods.length; i++) {
+        if(foods[i].checked){
+            food.push(foods[i]);
+        }
+    };
+    data.food = food;
+
+    
+
+
     let tableBody = document.querySelector('#table-body');
     let tableRow  = "" ;
     tableRow +=`
@@ -48,5 +54,10 @@ form.addEventListener('submit', function (event) {
     <td>${data.country}</td>
     </tr>`;
     tableBody.innerHTML = tableRow;
+    
+    let inputs = document.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.value = '';
+      });
 });
 
